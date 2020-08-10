8640f6fd35de0dfb7b04cd6357a156a9c914f25e
Andrew Bonas <aabonas@hotmail.com>
2020-08-10 16:22:24 -0700

    Updated user input

--- connect_four/spec/connect_four_spec.rb
+++ connect_four/spec/connect_four_spec.rb
@@ -9,26 +9,21 @@ describe Board do
       expect(subject.create_board.count).to be 6
       end
     end
-
-  describe '#update_display' do
-    xit 'Returns a user marker at the bottom to the bottom row of the display.'
-      expect(subject.update_display(1, X)).to eq(board[5][0])
-  end
 end
-    
-describe Player do
+
+describe Game do
   describe '#current_player' do 
     xit 'Atlernates between player 1 and player 2' do
       turn_count(3)
       expect(subject.update_player).to eq('O')
     end
   end
-end
-    
-describe Game do
+
   describe '#valid?' do
-    xit 'Returns invalid when the user chooses a number greater than 7' do
-    expect(subject.valid?(10)).to eq false
+    it 'Returns invalid when the user chooses a number greater than 7' do
+       game = Game.new
+        @input = 10
+    expect(game.valid?).to eq false
     end
   end
 end
